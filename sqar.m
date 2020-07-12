bf8fa7af4a53ee8ac0b5bb0f6c548b131ca73fb8
chetancae4 <66862140+chetancae4@users.noreply.github.com>
2020-07-12 14:21:07 +0530

    Add files via upload

---
+++ sqar.m
@@ -0,0 +1,28 @@
+## Copyright (C) 2020 HOME
+## 
+## This program is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see
+## <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {} {@var{retval} =} sqar (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: HOME <HOME@DESKTOP-N0UGELE>
+## Created: 2020-05-13
+
+function retval = sqar (input1, input2)
+
+endfunction
